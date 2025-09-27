ca74e4e4bf295e38b82df50e0d5ed58e435ca1b9
Devendra Dhakad <121676745+Devendradhakad12@users.noreply.github.com>
2025-09-27 23:41:47 +0530

    Remove redundant error handling for EPERM unlink in bootstrap function

--- src/index.js
+++ src/index.js
@@ -5,20 +5,5 @@ module.exports = {
 
   bootstrap(/*{ strapi }*/) {
     // Catch and ignore Windows EPERM unlink errors from temp file cleanup
-    process.on("uncaughtException", (err) => {
-      if (err.code === "EPERM" && err.syscall === "unlink") {
-        console.warn("Ignored EPERM unlink error:", err.path);
-        return;
-      }
-      throw err; // rethrow other errors
-    });
-
-    process.on("unhandledRejection", (err) => {
-      if (err && err.code === "EPERM" && err.syscall === "unlink") {
-        console.warn("Ignored EPERM unlink error:", err.path);
-        return;
-      }
-      throw err;
-    });
   },
 };
