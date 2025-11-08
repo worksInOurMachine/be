ca1704ba8c431c7aa730b0b5e981a44cd6f5fb8a
Devendra Dhakad <121676745+Devendradhakad12@users.noreply.github.com>
2025-11-09 02:50:05 +0530

    Add URL configuration to server settings

--- config/server.js
+++ config/server.js
@@ -2,6 +2,7 @@ module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   proxy: true,
+  url: "https://be-production-416a.up.railway.app",
   app: {
     keys: env.array("APP_KEYS"),
   },
