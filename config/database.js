e01a6de6060f1247c5d931643531695cd6d33600
Ajay Dhakad <ajaydhakad.py@gmail.com>
2025-11-09 03:03:37 +0530

    Refactor database configuration for clarity and restore commented proxy and URL settings in server configuration

--- config/database.js
+++ config/database.js
@@ -1,17 +1,17 @@
 const path = require("path");
 
-module.exports = ({ env }) => {
-  return {
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
     connection: {
-      client: "postgres",
-      connection: {
-        host: env("PGHOST", "127.0.0.1"),
-        port: env.int("PGPORT", 24569),
-        database: env("PGDATABASE", "strapi"),
-        user: env("PGUSER", "strapi"),
-        password: env("PGPASSWORD", "password"),
-        // ssl: true,
+      host: env("PGHOST", "127.0.0.1"),
+      port: env.int("PGPORT", 5432),
+      database: env("PGDATABASE", "strapi"),
+      user: env("PGUSER", "strapi"),
+      password: env("PGPASSWORD", "password"),
+      ssl: {
+        rejectUnauthorized: false,
       },
     },
-  };
-};
+  },
+});
--- config/server.js
+++ config/server.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  proxy: true,
-  url: "https://be-production-416a.up.railway.app",
+  // proxy: true,
+  // url: "https://be-production-416a.up.railway.app",
   app: {
     keys: env.array("APP_KEYS"),
   },
